7f7f304717ce98c11a28bb2413fa168d31543f10
paruj_lab <paruj.lab@gmail.com>
2022-11-18 14:20:03 +0700

    feat:setAvatar connect backend

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,13 +2,16 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Chat from "../pages/Chat";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import SetAvatar from "../pages/SetAvatar";
+// import SetAvatar from "../pages/SetAvatar";
 
 function Router() {
   return (
     <Routes>
         <Route path="/register" element={<Register/>}/>
         <Route path="/login" element={<Login/>}/>
-        <Route path="/chat" element={<Chat/>}/>
+        <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} />
     </Routes>
   )
 }
